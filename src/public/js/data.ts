458440682fd5847ccacae7048e9f354e0abfbc0d
John Knoll <jpknoll@gmail.com>
2018-03-06 16:06:08 -0800

    data context script

---
+++ src/public/js/data.ts
@@ -0,0 +1,77 @@
+const departments = require("./departments.json");
+const categories = require("./categories.json");
+const projects = require("./projects.json");
+
+// transform into array
+let sources = require("./sources.json");
+sources = Object.keys(sources).map((f) => {
+
+    // transform into array
+    const c = Object.keys(sources[f]).map(s => {
+        return {
+            name: s,
+            total: sources[f][s],
+        };
+    });
+
+    return {
+        name: f,
+        categories: c,
+        total: c.reduce((prev, d) => prev + d.total, 0),
+    };
+});
+
+export interface IProject {
+    project: string;
+    category: string;
+    dept: string;
+    total: number;
+}
+
+export interface ISource {
+    name: string;
+    categories: ICategory[];
+    total: number;
+}
+
+export interface ICategory {
+    name: string;
+    color?: string;
+    total?: number;
+}
+
+export function getProjects(): IProject[] {
+    return [...projects];
+}
+
+export function getSources(): ISource[] {
+    return [...sources];
+}
+
+export function getCategories(): ICategory[] {
+    return [...categories];
+}
+
+let selectedCategory = -1;
+export function getSelectedCategory(): number {
+    return selectedCategory;
+}
+
+export function setSelectedCategory(index: number): number {
+    selectedCategory = index;
+
+    events.forEach(e => {
+        e(index);
+    });
+
+    return selectedCategory;
+}
+
+export interface SelectedCategoryChangedEvent {
+    (index: number): void;
+}
+
+const events: SelectedCategoryChangedEvent[] = [];
+export function onSelectedCategoryChanged(func: SelectedCategoryChangedEvent) {
+    events.push(func);
+}
