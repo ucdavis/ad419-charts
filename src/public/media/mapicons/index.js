5228fb9e0b84f8d7ef7a32780189ec9f90602f6f
John Knoll <jpknoll@gmail.com>
2018-04-10 11:35:03 -0700

    map icons!

---
+++ src/public/media/mapicons/index.js
@@ -0,0 +1,18 @@
+export default {
+    bigwater: require('./bigwater.svg'),
+    brain: require('./brain.svg'),
+    cow: require('./cow.svg'),
+    crab: require('./crab.svg'),
+    dna: require('./dna.svg'),
+    droplets: require('./droplets.svg'),
+    grapes: require('./grapes.svg'),
+    kids: require('./kids.svg'),
+    leaf: require('./leaf.svg'),
+    marsh: require('./marsh.svg'),
+    milk: require('./milk.svg'),
+    orangeslice: require('./orangeslice.svg'),
+    seed: ('./seed.svg'),
+    strawberry: require('./strawberry.svg'),
+    tree: require('./tree.svg'),
+    wineglass: require('./wineglass.svg'),
+};
