16c05eacf6c4db3c5c308e7d8e871176f632a8cf
John Knoll <jpknoll@gmail.com>
2018-04-19 15:56:24 -0700

    hide legend on view all

--- src/public/js/legend.ts
+++ src/public/js/legend.ts
@@ -14,59 +14,49 @@ const projects = getProjects();
 const sumTotal = projects.reduce((prev, d) => prev + d.total, 0);
 
 // category change
-const totalChart = d3.selectAll("#bubble-summary-legend, #map-summary-legend");
+const legends = d3.selectAll("#bubble-summary-legend, #map-summary-legend");
 onSelectedCategoryChanged(categoryIndex => {
-  // update chart
+    console.log(categoryIndex);
   if (categoryIndex < 0) {
-    const total = sumTotal;
-    const count = projects.length;
-
-    totalChart.select(".icon");
-
-    totalChart.select(".title")
-        .text("");
-
-    totalChart.select(".total")
-        .text(`$${(total / 1000000).toFixed(1)}M`);
-
-    totalChart.select(".count")
-        .text(count);
-  } else {
-    const category = categories[categoryIndex];
-    const total = projects.reduce(
-      (prev, p) => (p.categoryIndex !== categoryIndex ? prev : prev + p.total),
-      0
-    );
-    const count = projects.reduce(
-      (prev, p) => (p.categoryIndex !== categoryIndex ? prev : prev + 1),
-      0
-    );
-
-    totalChart.select(".icon")
-        .attr("src", category.icon || "");
-
-    totalChart.select(".topic")
-        .text(category.name);
-
-    totalChart.select(".total")
-        .text(`$${(total / 1000000).toFixed(1)}M`);
-
-    totalChart.select(".count")
-        .text(count);
-
-    // remove all old departments
-    totalChart
-      .select(".departments")
-      .selectAll("p")
-      .remove();
-
-    const d = departments.filter(d => d.categoryIndex === categoryIndex);
-    totalChart
-      .select(".departments")
-      .selectAll("p")
-      .data(d)
-      .enter()
-      .append("p")
-      .text(d => d.name);
+    // hide legends
+    legends.classed("hidden", true);
+    return;
   }
+
+  const category = categories[categoryIndex];
+  const total = projects.reduce(
+    (prev, p) => (p.categoryIndex !== categoryIndex ? prev : prev + p.total),
+    0
+  );
+  const count = projects.reduce(
+    (prev, p) => (p.categoryIndex !== categoryIndex ? prev : prev + 1),
+    0
+  );
+
+  // show legend
+  legends.classed("hidden", false);
+
+  // update legend details
+  legends.select(".icon").attr("src", category.icon || "");
+
+  legends.select(".topic").text(category.name);
+
+  legends.select(".total").text(`$${(total / 1000000).toFixed(1)}M`);
+
+  legends.select(".count").text(count);
+
+  // remove all old departments
+  legends
+    .select(".departments")
+    .selectAll("p")
+    .remove();
+
+  const d = departments.filter(d => d.categoryIndex === categoryIndex);
+  legends
+    .select(".departments")
+    .selectAll("p")
+    .data(d)
+    .enter()
+    .append("p")
+    .text(d => d.name);
 });
