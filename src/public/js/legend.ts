0dc990753410b66f3cba170406dce56f51262298
John Knoll <jpknoll@gmail.com>
2018-05-10 14:29:40 -0700

    create all topic legend

--- src/public/js/legend.ts
+++ src/public/js/legend.ts
@@ -20,31 +20,51 @@ const sumTotal = projects.reduce((prev, d) => prev + d.total, 0);
 const legends = d3.selectAll("#bubble-summary-legend, #map-summary-legend");
 onSelectedCategoryChanged(categoryIndex => {
   if (categoryIndex < 0) {
-    // hide legends
-    legends.classed("d-none", true);
+    // set new title
+    legends.select(".title").text("CAES Research");
+
+    // hide icon
+    legends.select(".icon")
+      .classed("d-none", true);
+
+    // get totals, set text
+    const total = projects.reduce((prev, p) => prev + p.total, 0);
+    legends.select(".total").text(`$${(total / 1000000).toFixed(1)}M`);
+
+    // get count, set text
+    const count = projects.reduce((prev, p) => prev + 1, 0);
+    legends.select(".count").text(count);
+
+    // hide departments
+    legends
+      .select(".departments")
+      .classed("d-none", true);
+
     return;
   }
 
   const category = categories[categoryIndex];
+
+  // set new title
+  legends.select(".title").text(category.name);
+
+  // set icon or hide
+  legends.select(".icon")
+    .classed("d-none", false)
+    .attr("src", category.icon || "");
+
+  // get category total and set text
   const total = projects.reduce(
     (prev, p) => (p.categoryIndex !== categoryIndex ? prev : prev + p.total),
     0
   );
+  legends.select(".total").text(`$${(total / 1000000).toFixed(1)}M`);
+
+  // get category count and set text
   const count = projects.reduce(
     (prev, p) => (p.categoryIndex !== categoryIndex ? prev : prev + 1),
     0
   );
-
-  // show legend
-  legends.classed("d-none", false);
-
-  // update legend details
-  legends.select(".icon").attr("src", category.icon || "");
-
-  legends.select(".topic").text(category.name);
-
-  legends.select(".total").text(`$${(total / 1000000).toFixed(1)}M`);
-
   legends.select(".count").text(count);
 
   // remove all old departments
@@ -53,14 +73,16 @@ onSelectedCategoryChanged(categoryIndex => {
     .selectAll("p")
     .remove();
 
+  // show department container and add departments
   const d = departments.filter(d => d.categoryIndex === categoryIndex);
   legends
     .select(".departments")
+      .classed("d-none", false)
     .selectAll("p")
     .data(d)
     .enter()
     .append("p")
-    .text(d => d.name);
+      .text(d => d.name);
 });
 
 // setup circle scale legends
